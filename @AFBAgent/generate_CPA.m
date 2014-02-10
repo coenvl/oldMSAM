b611d43f58ba39f56ac14b81add8d79d2ed3cccb
C.J. van Leeuwen <coenvanl@gmail.com>
2014-02-10 16:44:54 +0100

    Remove matlab implementation

--- @AFBAgent/generate_CPA.m
+++
@@ -1,9 +0,0 @@
-function generate_CPA(obj)
-%GENERATE_CPA Summary of this function goes here
-%   Detailed explanation goes here
-
-%obj.cpa = uint8(zeros(1,obj.problem.numVars()));
-
-obj.cpa = uint8([]);
-end
-
--- @AFBAgent/init.m
+++
@@ -1,9 +0,0 @@
-function init(obj)
-obj.bound = Inf;  % line 1: Initialize with infinite upperbound
-
-if (obj.variableNum == 1)
-    obj.generate_CPA(); % line 3: First node will always start generating CPA
-    obj.assign_CPA(); % line 4: And after that it will assign the CPA
-end
-
-end
